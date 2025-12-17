596db72e1174a2d86973467ab5286aa28b8f4d1d
harish-bhati-ui <harishbhati.bhati@gmail.com>
2025-12-17 15:23:15 +0530

    api cross origin issue fix

--- src/api/api.js
+++ src/api/api.js
@@ -5,6 +5,7 @@ const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 const axiosInstance = axios.create({
   baseURL: API_BASE_URL,
   withCredentials: true, 
+  credentials: 'include'
   // send cookies if needed
 });
 
